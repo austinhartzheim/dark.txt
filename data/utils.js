e753a857b6d4b0e55ee0200d1a889faab64d5824
Austin Hartzheim <austin@austinhartzheim.me>
2015-08-14 06:18:22 +0000

    Use the DOM classList instead of regular expressions.

--- data/utils.js
+++ data/utils.js
@@ -9,11 +9,10 @@ self.port.on("attachCss", function(url) {
 
 self.port.on("toggleColor", function() {
     // Toggle the class to enable/disable the CSS theme.
-    if (document.body.className.match(/(?:^|\s)darktxt-enabled(?!\S)/)) {
-	document.body.className = document.body.className
-	    .replace(/(?:^|\s)darktxt-enabled(?!\S)/g , '');
+    if (document.body.classList.contains('darktxt-enabled')) {
+	document.body.classList.remove('darktxt-enabled');
     } else {
-	document.body.className += " darktxt-enabled";
+	document.body.classList.add('darktxt-enabled');
     }
 });
 
