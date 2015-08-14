db64df5a52a5b4763e96c5cad8dbe570584c4b74
Austin Hartzheim <austin@austinhartzheim.me>
2015-08-14 23:23:47 +0000

    Added support for alternate color themes.

--- data/utils.js
+++ data/utils.js
@@ -7,12 +7,19 @@ self.port.on("attachCss", function(url) {
     document.head.appendChild(style);
 });
 
-self.port.on("toggleColor", function() {
+self.port.on("toggleColor", function(themeclass) {
     // Toggle the class to enable/disable the CSS theme.
     if (document.body.classList.contains('darktxt-enabled')) {
 	document.body.classList.remove('darktxt-enabled');
+	document.body.classList.remove('darktxt-whiteblack');
+	document.body.classList.remove('darktxt-greenblack');
+	document.body.classList.remove('darktxt-pinkblack');
+	document.body.classList.remove('darktxt-yellowblack');
+	document.body.classList.remove('darktxt-redblack');
+	document.body.classList.remove('darktxt-whitegray');
     } else {
 	document.body.classList.add('darktxt-enabled');
+	document.body.classList.add(themeclass);
     }
 });
 
